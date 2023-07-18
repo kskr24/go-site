29cbdc76d06b00015eed6d7eb6c46635a1cae62e
Shekhar Kumar <sk7193958@gmail.com>
2023-07-19 03:07:02 +0530

    Use LF line endings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,1 +1,16 @@
-package mainimport (	"fmt"	"github.com/kskr24/go-site/pkg/handlers"	"net/http")const portNumber = ":8080"func main() {	http.HandleFunc("/", handlers.Home)	http.HandleFunc("/about", handlers.About)	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))	_ = http.ListenAndServe(portNumber, nil)}
+package main
+
+import (
+	"fmt"
+	"github.com/kskr24/go-site/pkg/handlers"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
