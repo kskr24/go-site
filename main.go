1bae40d96c6950b2e4bb0bc4458eb3ff1e85f80c
Shekhar Kumar <sk7193958@gmail.com>
2023-10-26 22:53:23 +0530

    Rework on the files

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the about page")
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	_ = http.ListenAndServe(":8080", nil)
+}
